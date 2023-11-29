91f47f792d482fad8dd323f5013cbc89c6bc2c2b
MatthewMalekP <matthewmalekp@gmail.com>
2023-11-29 03:11:38 +0000

    Mrowa ATTACK

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    private NavMeshAgent navmMeshAgent;
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navmMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FoodSource food = FindObjectOfType<FoodSource>();
+
+        NavMeshHit hit;
+        bool hasHit = NavMesh.SamplePosition(food.transform.position, out hit, 2f, NavMesh.AllAreas);
+
+        if (hasHit)
+        {
+            navmMeshAgent.SetDestination(hit.position);
+        }
+
+        animator.SetFloat("Speed", navmMeshAgent.velocity.magnitude);
+
+        if (Vector3.Distance(hit.position, transform.position) < 1f)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+}
